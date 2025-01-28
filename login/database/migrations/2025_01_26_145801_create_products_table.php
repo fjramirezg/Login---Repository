ee2469ec0467140d20980d705a9e4a93c59264af
Frank Ramirez <frank.johanra@hotmail.com>
2025-01-28 10:58:17 -0500

    Issue Renames_categories

--- login/database/migrations/2025_01_26_145801_create_products_table.php
+++ login/database/migrations/2025_01_26_145801_create_products_table.php
@@ -10,13 +10,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('category_id')->nullable();//categories_id
+            $table->unsignedBigInteger('category_id')->nullable();
             $table->string('nombre', 100);
             $table->decimal('precio', 10, 2);
             $table->integer('cantidad')->default(0);
             $table->timestamps();
 
-            // FK a categories con ON DELETE SET NULL
             $table->foreign('category_id')
                 ->references('id')
                 ->on('categories')
